e7f1d5d2ecfdbc82a983a20ad748cb0a77bfa837
guilleballesteros <bugfiher@outlook.com>
2021-02-10 12:12:56 +0100

    cambios actuales

---
+++ src/main/java/com/example/demo/model/CarritoModel.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+public class CarritoModel {
+	
+	private MedicamentoModel medicamento;
+	private int num;
+	public CarritoModel(MedicamentoModel medicamento, int num) {
+		this.medicamento = medicamento;
+		this.num = num;
+	}
+	
+	public CarritoModel() {}
+	
+	
+
+}
